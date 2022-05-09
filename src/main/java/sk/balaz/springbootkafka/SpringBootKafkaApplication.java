e9ab0bd00d7cac1bf4004e942fbf5bfcc56d0fd7
Tomas Balaz <tomasba@amdocs.com>
2022-05-09 13:26:59 +0200

    Created KafkaProducerConfig and send message via KafkaTemplate to Kafka broker

--- src/main/java/sk/balaz/springbootkafka/SpringBootKafkaApplication.java
+++ src/main/java/sk/balaz/springbootkafka/SpringBootKafkaApplication.java
@@ -1,13 +1,23 @@
 package sk.balaz.springbootkafka;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
 public class SpringBootKafkaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootKafkaApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(KafkaTemplate<String, String> kafkaTemplate) {
+        return args -> {
+            kafkaTemplate.send("topic", "hello kafka");
+        };
     }
 
 }
